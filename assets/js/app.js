b5fd94f236ba1d2e35a7842683f46be7480c2dcc
Tree Ant <stokes.finance@gmail.com>
2020-04-05 23:59:40 -0500

    added list

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,8 @@ var songList1EL = document.querySelector("#songList1");
 var songList2EL = document.querySelector("#songList2");
 var songList3EL = document.querySelector("#songList3");
 var songList4EL = document.querySelector("#songList4");
+var songList5EL = document.querySelector("#songList5");
+
 
 const BGs = [
     "./assets/imgs/BGs/asparagus.jpg", "./assets/imgs/BGs/axe.jpg	", "./assets/imgs/BGs/backgrounds.jpg", "./assets/imgs/BGs/banana.jpg", "./assets/imgs/BGs/baseballman.jpg", "./assets/imgs/BGs/bass.jpg", "./assets/imgs/BGs/beer.jpg", "./assets/imgs/BGs/bike.jpg", "./assets/imgs/BGs/bird.jpg	", "./assets/imgs/BGs/camera.jpg", "./assets/imgs/BGs/casket.jpg", "./assets/imgs/BGs/chess.jpg", "./assets/imgs/BGs/chicken.jpg", "./assets/imgs/BGs/clav.jpg", "./assets/imgs/BGs/coffee.jpg", "./assets/imgs/BGs/contractor.jpg", "./assets/imgs/BGs/corn.jpg", "./assets/imgs/BGs/cowbell.jpg", "./assets/imgs/BGs/cp70.jpg", "./assets/imgs/BGs/drill.jpg", "./assets/imgs/BGs/drums.jpg", "./assets/imgs/BGs/duck.jpg", "./assets/imgs/BGs/elephant.jpg", "./assets/imgs/BGs/evilrob.jpg", "./assets/imgs/BGs/farfisa.jpg", "./assets/imgs/BGs/fieri.jpg", "./assets/imgs/BGs/fireplace.jpg", "./assets/imgs/BGs/footballplayer.jpg", "./assets/imgs/BGs/garbage.jpg", "./assets/imgs/BGs/greenonions.jpg", "./assets/imgs/BGs/hammond.jpg", "./assets/imgs/BGs/harold.jpg", "./assets/imgs/BGs/hatchet.jpg", "./assets/imgs/BGs/hotdog.jpg", "./assets/imgs/BGs/ketchup.jpg", "./assets/imgs/BGs/knife.jpg", "./assets/imgs/BGs/lambo.jpg", "./assets/imgs/BGs/martini.jpg", "./assets/imgs/BGs/mic.jpg", "./assets/imgs/BGs/monkey.jpg", "./assets/imgs/BGs/nickcage.jpg", "./assets/imgs/BGs/ps3knockoffcontroller.jpg", "./assets/imgs/BGs/regulardog.jpg", "./assets/imgs/BGs/rhodes.jpg", "./assets/imgs/BGs/rob.jpg", "./assets/imgs/BGs/robtrent.jpg", "./assets/imgs/BGs/sax.jpg", "./assets/imgs/BGs/shovel.jpg", "./assets/imgs/BGs/spider.jpg", "./assets/imgs/BGs/straightblue.jpg", "./assets/imgs/BGs/sword.jpg", "./assets/imgs/BGs/trentwater.jpg", "./assets/imgs/BGs/twoaxes.jpg", "./assets/imgs/BGs/watch.jpg", "./assets/imgs/BGs/waterbottle.jpg", "./assets/imgs/BGs/weight.jpg", "./assets/imgs/BGs/whiskey.jpg", "./assets/imgs/BGs/wurly.jpg" 
@@ -478,6 +480,467 @@ const songTITLES4 = [
     artist: "dan was",
   },
 ];
+
+const songTITLES5 = [
+    {
+        title:  "all in a life",
+        artist: "robert ledet"
+        },
+        {
+        title:  "all that matters (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "all will be lost",
+        artist: "robert ledet"
+        },
+        {
+        title:  "alone",
+        artist: "robert ledet"
+        },
+        {
+        title:  "around her",
+        artist: "robert ledet"
+        },
+        {
+        title:  "away",
+        artist: "robert ledet"
+        },
+        {
+        title:  "bad girls",
+        artist: "robert ledet"
+        },
+        {
+        title:  "bank robbery (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "be my love",
+        artist: "robert ledet"
+        },
+        {
+        title:  "better off alone",
+        artist: "robert ledet"
+        },
+        {
+        title:  "big wheels",
+        artist: "robert ledet"
+        },
+        {
+        title:  "by the sea",
+        artist: "robert ledet"
+        },
+        {
+        title:  "call it a maybe (cowrite Nick Thomas low)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "can’t say i didn’t try",
+        artist: "robert ledet"
+        },
+        {
+        title:  "clearer",
+        artist: "robert ledet"
+        },
+        {
+        title:  "closer to you",
+        artist: "robert ledet"
+        },
+        {
+        title:  "could you be mine (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "cry your way to heaven (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "don’t say goodnight (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "don’t wanna live without you (cowrite JJ Maloney)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "don’t worry",
+        artist: "robert ledet"
+        },
+        {
+        title:  "dreams (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "easy to fall",
+        artist: "robert ledet"
+        },
+        {
+        title:  "empire",
+        artist: "robert ledet"
+        },
+        {
+        title:  "enter the ground (cowrite JJ Maloney)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "far from here",
+        artist: "robert ledet"
+        },
+        {
+        title:  "finding the truth to love",
+        artist: "robert ledet"
+        },
+        {
+        title:  "friday nights",
+        artist: "robert ledet"
+        },
+        {
+        title:  "get back there (cowrite matt miller)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "getting older",
+        artist: "robert ledet"
+        },
+        {
+        title:  "girl on fire (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "goodbye to my lady (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "got this far (cowrite Nick Thomas low)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "heartstrings",
+        artist: "robert ledet"
+        },
+        {
+        title:  "hey hey",
+        artist: "robert ledet"
+        },
+        {
+        title:  "hiding from memories",
+        artist: "robert ledet"
+        },
+        {
+        title:  "i’m gone",
+        artist: "robert ledet"
+        },
+        {
+        title:  "i’ve been running away",
+        artist: "robert ledet"
+        },
+        {
+        title:  "juliane",
+        artist: "robert ledet"
+        },
+        {
+        title:  "just give me time",
+        artist: "robert ledet"
+        },
+        {
+        title:  "keep a little tough",
+        artist: "robert ledet"
+        },
+        {
+        title:  "let it fall",
+        artist: "robert ledet"
+        },
+        {
+        title:  "let’s go dancing",
+        artist: "robert ledet"
+        },
+        {
+        title:  "like i should",
+        artist: "robert ledet"
+        },
+        {
+        title:  "livin’ a real lie",
+        artist: "robert ledet"
+        },
+        {
+        title:  "looks like a weapon",
+        artist: "robert ledet"
+        },
+        {
+        title:  "missing you",
+        artist: "robert ledet"
+        },
+        {
+        title:  "here i am",
+        artist: "robert ledet"
+        },
+        {
+        title:  "hold on",
+        artist: "robert ledet"
+        },
+        {
+        title:  "hospital",
+        artist: "robert ledet"
+        },
+        {
+        title:  "if i walk away (cowrite laura leigh)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "if you need a friend",
+        artist: "robert ledet"
+        },
+        {
+        title:  "illusions",
+        artist: "robert ledet"
+        },
+        {
+        title:  "in the evening",
+        artist: "robert ledet"
+        },
+        {
+        title:  "in your atmosphere",
+        artist: "robert ledet"
+        },
+        {
+        title:  "let me",
+        artist: "robert ledet"
+        },
+        {
+        title:  "let me feel your love",
+        artist: "robert ledet"
+        },
+        {
+        title:  "let me go home (cowrite Mitchell Sullivan)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "life keeps you runnin’",
+        artist: "robert ledet"
+        },
+        {
+        title:  "never quite over (cowrite JJ Maloney)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "on my way back down",
+        artist: "robert ledet"
+        },
+        {
+        title:  "on the seashore",
+        artist: "robert ledet"
+        },
+        {
+        title:  "mississippi fool (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "movin’ on",
+        artist: "robert ledet"
+        },
+        {
+        title:  "mud",
+        artist: "robert ledet"
+        },
+        {
+        title:  "my room",
+        artist: "robert ledet"
+        },
+        {
+        title:  "my town (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "night drive (turbobird – cowrite nick Purvis)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "no control",
+        artist: "robert ledet"
+        },
+        {
+        title:  "not so perfect world",
+        artist: "robert ledet"
+        },
+        {
+        title:  "now or someday",
+        artist: "robert ledet"
+        },
+        {
+        title:  "ocean (from RLEP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "other side of the sunset",
+        artist: "robert ledet"
+        },
+        {
+        title:  "parking brakes",
+        artist: "robert ledet"
+        },
+        {
+        title:  "passenger",
+        artist: "robert ledet"
+        },
+        {
+        title:  "piece of my love",
+        artist: "robert ledet"
+        },
+        {
+        title:  "plans can change",
+        artist: "robert ledet"
+        },
+        {
+        title:  "play for free (from guttersoul EP)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "sail along",
+        artist: "robert ledet"
+        },
+        {
+        title:  "second time fills you up",
+        artist: "robert ledet"
+        },
+        {
+        title:  "settle for him",
+        artist: "robert ledet"
+        },
+        {
+        title:  "she is just like the ocean",
+        artist: "robert ledet"
+        },
+        {
+        title:  "shoot your shot (from play for free film soundtrack)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "shy",
+        artist: "robert ledet"
+        },
+        {
+        title:  "sky",
+        artist: "robert ledet"
+        },
+        {
+        title:  "so far past midnight",
+        artist: "robert ledet"
+        },
+        {
+        title:  "so funky",
+        artist: "robert ledet"
+        },
+        {
+        title:  "so much for this – new song!",
+        artist: "robert ledet"
+        },
+        {
+        title:  "so much is wasted",
+        artist: "robert ledet"
+        },
+        {
+        title:  "stabilize",
+        artist: "robert ledet"
+        },
+        {
+        title:  "stars (cowrite Aaron De La Fuente)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "stop spinnin’",
+        artist: "robert ledet"
+        },
+        {
+        title:  "summer is a long time",
+        artist: "robert ledet"
+        },
+        {
+        title:  "sweet hesitation",
+        artist: "robert ledet"
+        },
+        {
+        title:  "take it easy",
+        artist: "robert ledet"
+        },
+        {
+        title:  "take you out after midnight",
+        artist: "robert ledet"
+        },
+        {
+        title:  "talk to me",
+        artist: "robert ledet"
+        },
+        {
+        title:  "everything sounds cool on this thing (cowrite trent stokes)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "three am",
+        artist: "robert ledet"
+        },
+        {
+        title:  "tonight it’s fine",
+        artist: "robert ledet"
+        },
+        {
+        title:  "two more hours",
+        artist: "robert ledet"
+        },
+        {
+        title:  "urgent (turbobird – cowrite nick Purvis)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "we can’t pretend",
+        artist: "robert ledet"
+        },
+        {
+        title:  "weakness",
+        artist: "robert ledet"
+        },
+        {
+        title:  "what you’re saying",
+        artist: "robert ledet"
+        },
+        {
+        title:  "whatcha gonna do",
+        artist: "robert ledet"
+        },
+        {
+        title:  "when i knew you before (cowrite JJ Maloney)",
+        artist: "robert ledet"
+        },
+        {
+        title:  "when you get bored – new song!",
+        artist: "robert ledet"
+        },
+        {
+        title:  "where you gonna go after christmas",
+        artist: "robert ledet"
+        },
+        {
+        title:  "why do i love you in the nighttime?",
+        artist: "robert ledet"
+        },
+        {
+        title:  "yellow",
+        artist: "robert ledet"
+        },
+        {
+        title:  "you don’t have to wait",
+        artist: "robert ledet"
+        },
+        {
+        title:  "you poor creature",
+        artist: "robert ledet"
+        },
+        {
+        title:  "you put the fire in me",
+        artist: "robert ledet"
+        }
+        
+];
+
 const usedSongLISTs = ["songTITLES3", "songTITLES1", "songTITLES2"];
 
 spin = () => {
@@ -538,6 +1001,13 @@ listMAKER = () => {
     }
   } else {
     console.log("fail-4");
+  }
+  if (songList5EL.checked === true) {
+    for (let i = 0; i < songTITLES5.length; i++) {
+      usedSongTITLES.push(songTITLES5[i]);
+    }
+  } else {
+    console.log("fail-5");
   }
 };
 
